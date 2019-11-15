12e7445e897a5ebb1023961f7359398a50f4e510
xian-xian <1099693113@qq.com>
2019-11-15 16:45:05 +0800

    first

---
+++ src/HomePage/homepage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Icon } from 'antd';
+import {Link} from 'react-router-dom'
+
+var HomepageCss = require('./homepage.css')
+
+export default class Homepage extends React.Component {
+    render() {
+        return (
+            <div className={HomepageCss.all}>
+                
+            </div>
+        )
+    }
+}
--- src/index.js
+++ src/index.js
@@ -2,22 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Homepage from './HomePage/homepage'
 import * as serviceWorker from './serviceWorker';
+
+import {BrowserRouter,Route,Switch,Redirect} from 'react-router-dom'
 
 
 export default class Root extends React.Component{
     render(){
         return(
             <div>
+                 <BrowserRouter basename='vacation'>
+                 <Switch>
+                 <Route path='/' exact component={Homepage}></Route>
                
+                </Switch>
+                </BrowserRouter>
                
             </div>
         )
     }
 }
-
-
-ReactDOM.render(<App />, document.getElementById('root'));
+  
+ReactDOM.render(<Root />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.

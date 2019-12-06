850d24ecd8b893d0966da4c0251cf64a588a0257
Anmicius-01 <1592241827@qq.com>
2019-12-06 14:54:12 +0800

    轮播图

--- src/HomePage/homepage.js
+++ src/HomePage/homepage.js
@@ -1,6 +1,9 @@
 import React from 'react'
-import { Row, Col, Icon, Menu, Dropdown, Input, Avatar, Button, Popconfirm, message } from 'antd'
+import { Row, Col, Icon, Menu, Dropdown, Input, Avatar, Button, Popconfirm, message, Carousel } from 'antd'
 import { Link } from 'react-router-dom'
+import img1 from '../Image/养老院.jpg'
+import img2 from '../Image/孤儿院.jpg'
+import img3 from '../Image/福利院.jpg'
 
 var HomepageCss = require("../HomePage/homepage.css")
 
@@ -53,6 +56,26 @@ const menu1 = (
     </Menu>
 );
 
+
+// const bgGround1 = {
+//     display: 'inline-block',
+//     height: '600px',
+//     width: '100%',
+//     background: 'url(' + img1 + ')'
+// }
+// const bgGround2 = {
+//     display: 'inline-block',
+//     height: '600px',
+//     width: '100%',
+//     background: 'url(' + img2 + ')'
+// }
+// const bgGround3 = {
+//     display: 'inline-block',
+//     height: '600px',
+//     width: '100%',
+//     background: 'url(' + img3 + ')'
+// }
+
 export default class Homepage extends React.Component {
 
     render() {
@@ -66,7 +89,7 @@ export default class Homepage extends React.Component {
                         <Col span={2}>
                             <Dropdown overlay={menu}>
                                 <Link className="ant-dropdown-link" trigger={['click']}>
-                                    <Icon type="unordered-list" style={{fontSize:'30px'}}/>
+                                    <Icon type="unordered-list" style={{ fontSize: '30px' }} />
                                 </Link>
                             </Dropdown>
                         </Col>
@@ -98,7 +121,62 @@ export default class Homepage extends React.Component {
                         </Col>
                     </Row>
                 </div>
-                <div></div>
+                <div className={HomepageCss.introduce}>
+                    <div className={HomepageCss.imgs}>
+                        <Carousel autoplay>
+                            <div className={HomepageCss.img1}>
+                                <div className={HomepageCss.write1}>
+                                    <Row>
+                                        <Col span={2}></Col>
+                                        <Col span={6}></Col>
+                                        <Col span={8}></Col>
+                                        <Col span={8}>
+                                            <p><strong>Volunteers</strong></p>
+                                            <Button type="primary">efcsdfsad</Button>
+                                        </Col>
+                                    </Row>
+                                </div>
+                            </div>
+                            <div>
+                                <div className={HomepageCss.img2}></div>
+                            </div>
+                            <div>
+                                <div className={HomepageCss.img3}></div>
+                            </div>
+                            <div>
+                                <div className={HomepageCss.img4}></div>
+                            </div>
+                        </Carousel>
+                    </div>
+                </div>
+                <div className={HomepageCss.card}>
+                    <Row>
+                        <Col span={2}></Col>
+                        <Col span={6}>
+                            <div className={HomepageCss.img1}>
+                                <div className={HomepageCss.hide1}>
+                                    shuiasdhia
+                                </div>
+                                <div className={HomepageCss.picture}>
+                                    <img src={img1} height={600} width={380}></img>
+                                </div>
+                            </div>
+                        </Col>
+                        <Col span={1}></Col>
+                        <Col span={6}>
+                            <div className={HomepageCss.img1}>
+                                <img src={img2} height={600} width={380} className={HomepageCss.picture}></img>
+                            </div>
+                        </Col>
+                        <Col span={1}></Col>
+                        <Col span={6}>
+                            <div className={HomepageCss.img1}>
+                                <img src={img3} height={600} width={380}></img>
+                            </div>
+                        </Col>
+                        <Col span={2}></Col>
+                    </Row>
+                </div>
                 <div></div>
             </div>
         )

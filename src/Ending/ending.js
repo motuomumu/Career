db5e28acfd891db8eb9f56ce3ec9639288334941
xian-xian <1099693113@qq.com>
2020-04-23 13:28:06 +0800

    finally

--- src/Ending/ending.js
+++ src/Ending/ending.js
@@ -11,14 +11,14 @@ export default class Ending extends React.Component {
             <div className={endCss.all}>
                 <Result
                     status="success"
-                    title="已成功报名该志愿活动"
-                    subTitle="感谢您的积极参与。
+                    title="已成功提交申请"
+                    subTitle="
                     请在之后的7个工作日内注意接收信息。"
                     extra={[
                         <Button type="primary" key="console">
                             <Link to="/user/center">返回个人中心</Link>
                         </Button>,
-                        <Button key="buy"><Link to="/user/details">返回活动页面</Link></Button>,
+                        <Button key="buy"><Link to="/user/details">返回招聘</Link></Button>,
                     ]}
                 />
             </div>
